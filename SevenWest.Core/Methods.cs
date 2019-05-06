f8ba64c3dc9d85038c4e12dda3a0c26dba238dc8
Daniel Fell <daniel.fell@hotelscombined.com>
2019-05-06 16:06:24 +1000

    Bare bones structure and repo init

---
+++ SevenWest.Core/Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace SevenWest.Core
+{
+    public class Methods
+    {
+        // TODO Methods here
+    }
+
+    public class MethodsTest
+    {
+        // TODO Tests here 
+        // TODO Move tests out
+    }
+}
