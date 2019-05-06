781833f61dd0ea5f5a8d0a902dc6841d8e51c1b5
Daniel Fell <daniel.fell@hotelscombined.com>
2019-05-06 18:24:25 +1000

    Added Person.cs. Can deserialize properly now.

---
+++ SevenWest.Core/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SevenWest.Core
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string First { get; set; }
+        public string Last { get; set; }
+        public int Age { get; set; }
+        public char Gender { get; set; }
+    }
+}
--- SevenWest.Test/MethodsTest.cs
+++ SevenWest.Test/MethodsTest.cs
@@ -1,6 +1,10 @@
 using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 using Shouldly;
 using Xunit;
+using System.Linq;
+using SevenWest.Core;
 
 namespace SevenWest.Test
 {
@@ -13,33 +17,39 @@ namespace SevenWest.Test
         }
 
         private string jsonString =
-            @"[{ ""id"": 53, ""first"": ""Bill"", ""last"": ""Bryson"", ""age"":23, ""gender"":""M"" },
-                        { ""id"": 62, ""first"": ""John"", ""last"": ""Travolta"", ""age"":54, ""gender"":""M"" },
-                        { ""id"": 41, ""first"": ""Frank"", ""last"": ""Zappa"", ""age"":23, gender:""T"" },
-                        { ""id"": 31, ""first"": ""Jill"", ""last"": ""Scott"", ""age"":66, gender:""Y"" },
-                        { ""id"": 31, ""first"": ""Anna"", ""last"": ""Meredith"", ""age"":66, ""gender"":""Y"" },
-                        { ""id"": 31, ""first"": ""Janet"", ""last"": ""Jackson"", ""age"":66, ""gender"":""F"" }]";
+            @"[{ 'id': 53, 'first': 'Bill', 'last': 'Bryson', 'age':23, 'gender':'M' },
+                        { 'id': 62, 'first': 'John', 'last': 'Travolta', 'age':54, 'gender':'M' },
+                        { 'id': 41, 'first': 'Frank', 'last': 'Zappa', 'age':23, gender:'T' },
+                        { 'id': 31, 'first': 'Jill', 'last': 'Scott', 'age':66, gender:'Y' },
+                        { 'id': 31, 'first': 'Anna', 'last': 'Meredith', 'age':66, 'gender':'Y' },
+                        { 'id': 31, 'first': 'Janet', 'last': 'Jackson', 'age':66, 'gender':'F' }]";
+
+        private List<Person> StubDataInput()
+        {
+            return JsonConvert.DeserializeObject<List<Person>>(jsonString);
+        }
 
         // TODO Tests here 
         [Fact]
-        public void ShouldBeValidJSONInput()
+        public void ShouldBeValidGenderEntries()
         {
             // Arrange
-            var tmp = "test";
+            var inputData = StubDataInput();
+            bool allGendersAreValid = !inputData.Any(x => x.Gender != 'M' || x.Gender != 'F');
 
             // Act
 
-            //dynamic json = JsonConvert.DeserializeObject(jsonString);
-            //json.ShouldNotBeNullOrEmpty();
 
             // Assert
-            tmp.ShouldNotBeNullOrEmpty();
+            allGendersAreValid.ShouldBeTrue();
+
+
 
 
         }
 
         [Fact]
-        public void InputFieldsAreCorrect()
+        public void ShouldBeInputFieldsAreCorrect()
         {
             // Arrange
             var code = 4;
